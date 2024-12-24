23749ce0dd1137cb91d9b065d5e4d03cea5c9073
mdev34-lab <117395510+mdev34-lab@users.noreply.github.com>
2024-12-23 22:21:51 -0300

    Second hotfix update btw =)

--- utils.py
+++ utils.py
@@ -1,6 +1,19 @@
 from moviepy import *
 from PIL import Image
 import os
+import edge_tts
+import tempfile
+
+def remove_affix(s: str, affixes: tuple) -> str:
+    """Remove prefixes and suffixes from a string."""
+    # Strip whitespace and ensure we handle unexpected characters
+    # print(s)
+    for affix in affixes:
+        if s.startswith(affix):
+            s = s[len(affix):]
+        if s.endswith(affix):
+            s = s[:-len(affix)]
+    return s
 
 def get_character_images(sprites: dict, character_folder: str):
     character_images: tuple = tuple()
@@ -25,9 +38,19 @@ class WhiteClip(ColorClip):
     def __init__(self, size, *args, **kwargs):
         super().__init__(size=size, color=(255, 255, 255), *args, **kwargs)
 
+class BlackClip(ColorClip):
+    def __init__(self, size, *args, **kwargs):
+        super().__init__(size=size, color=(0, 0, 0), *args, **kwargs)
+
 class CharacterClip(ImageClip):
     def __init__(self, character, CHARACTERS: tuple, CHARACTER_IMAGES_FOLDER: str, *args, **kwargs):
         if character not in CHARACTERS:
             raise ValueError(f"Character '{character}' not in {CHARACTERS}")
         super().__init__(os.path.join(CHARACTER_IMAGES_FOLDER, f"{character}.png"), *args, **kwargs)
         self.character = character
+
+async def edge_tts_to_audiofile(text: str, voice: str, temp_audio_file_path: str) -> str:
+    """Retrieve TTS audio from edge_tts and save to a temporary file."""
+    communicate = edge_tts.Communicate(text, voice)
+    await communicate.save(temp_audio_file_path)
+    return temp_audio_file_path
